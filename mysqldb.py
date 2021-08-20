87a76b195049431178f113a647581669e1c09a45
moony <bestyyhb@gmail.com>
2021-08-20 23:28:28 +0800

    insertalbe

---
+++ mysqldb.py
@@ -0,0 +1,14 @@
+import pymysql
+
+
+class MySQL():
+    __init__(self):
+    def db(self,check_data):
+        sql = "INSERT INTO SIGN.paofu_checkin (now(),{})".format(check_data)
+        #执行SQL语句
+
+if __name__=="__main__":
+    test=MySQL()
+    test.db(543)
+    sql = "SELECT DATA FROM SIGN.paofu_checkin WHERE DATE LIKE '%hour(now())%' "
+    commit(sql)
